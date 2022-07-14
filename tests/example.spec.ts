7c6c91283d5bd777c331dca3241901d3787b02ce
nikhilesh009 <nikhil.etc@gmail.com>
2022-07-14 08:21:24 +0200

    init

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,10 @@
-import { test, expect } from '@playwright/test';
-import { SignUp } from './signup/register';
+import { test } from '@playwright/test';
+import { SignUp } from './signup/lib/register';
 
-test('basic test', async ({ page }) => {
-  // await page.goto('https://playwright.dev/');
-  await SignUp.
-  const title = page.locator(attribute.nav);
-  await expect(title).toHaveText('Playwright');
+test('create new registration', async ({ page }) => {
+  const signup = new SignUp(page);
+  await signup.visitPaybackRegistrationPage();
+  await signup.selectNewCardVariant();
+  await signup.selectCardType('atBPWelcome');
+  await signup.waiter();
 });
-
-const attribute = {
-  nav: '.navbar__inner .navbar__title'
-};
---
+++ tests/signup/lib/register.ts
@@ -0,0 +1,59 @@
+import type { Page } from 'playwright';
+import { faker } from '@faker-js/faker';
+import { expect } from '@playwright/test';
+
+export const registrationSelector = {
+  cardNumber: 'input[id="cardnumber"]',
+  cvvNumber: '[id="cvc"]',
+  newCardVariant: 'label.pb-radio__label',
+  acceptCookies: '[id="onetrust-accept-btn-handler"]',
+  signUpButton:
+    '[class="row pb-sign-up-block-container"] [class="pb-sign-up__button-text"]',
+  signUpEmail: '[id="email"]',
+  password: '[type="password"]',
+  errorMsg: '[class*="pb-form-field__error-msg_hidden"]'
+};
+
+export class SignUp {
+  readonly page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async acceptCookies() {
+    await this.page.locator(registrationSelector.acceptCookies).click();
+  }
+
+  async visitPaybackRegistrationPage() {
+    await this.page.goto('/anmelden');
+    await this.acceptCookies();
+  }
+
+  async setCardNumber(cardNumber: string) {
+    await this.page.fill(registrationSelector.cardNumber, cardNumber);
+  }
+
+  async setCvvNumber(cvvNumber: string) {
+    await this.page.fill(registrationSelector.cvvNumber, cvvNumber);
+  }
+
+  async selectNewCardVariant() {
+    await this.page.locator(registrationSelector.newCardVariant).last().click();
+  }
+
+  async selectCardType(cardType: string) {
+    await this.page.locator(`[data-id="${cardType}"]`).first();
+  }
+  async waiter() {
+    await this.page.locator(registrationSelector.signUpButton).first().click();
+  }
+  async setSignUpEmail() {
+    const randomEmail = faker.internet.email();
+    await this.page.fill(registrationSelector.signUpEmail, randomEmail);
+    await expect(registrationSelector.errorMsg).toHaveLength(0);
+  }
+  async setPassword() {
+    await this.page.fill(registrationSelector.signUpEmail, '1234');
+    await expect(registrationSelector.errorMsg).toHaveLength(0);
+  }
+}
